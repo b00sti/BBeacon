a373f60756b8bad29bfa8be35681d04bbc66c4ed
b00sti <pawlik.dominik@gmail.com>
2017-03-06 13:17:56 +0100

    add ble handling

---
+++ app/src/main/java/com/example/b00sti/bbeacon/bluetooth/CannotConnectException.java
@@ -0,0 +1,11 @@
+package com.example.b00sti.bbeacon.bluetooth;
+
+/**
+ * Created by b00sti on 16.01.2017.
+ */
+
+public class CannotConnectException extends Exception {
+    public CannotConnectException(String message) {
+        super(message);
+    }
+}
