2af6a4f78a6ad866cd26327745774f1c64566690
b00sti <pawlik.dominik@gmail.com>
2017-03-10 13:49:06 +0100

    added UI to alarm

--- app/src/main/java/com/example/b00sti/bbeacon/ui_alarm/AlarmItemView.java
+++ app/src/main/java/com/example/b00sti/bbeacon/ui_alarm/AlarmItemView.java
@@ -1,13 +1,19 @@
 package com.example.b00sti.bbeacon.ui_alarm;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.b00sti.bbeacon.R;
 import com.example.b00sti.bbeacon.base.BaseItemView;
+import com.example.b00sti.bbeacon.utils.SwitchButton;
 
 import org.androidannotations.annotations.EViewGroup;
 import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.res.IntArrayRes;
+
+import java.util.Random;
 
 /**
  * Created by Dominik (b00sti) Pawlik on 2017-03-09
@@ -17,6 +23,14 @@ import org.androidannotations.annotations.ViewById;
 public class AlarmItemView extends BaseItemView<AlarmItem> {
 
     @ViewById(R.id.layout_item_demo_title) TextView textView;
+    @ViewById(R.id.sidebar) View sidebar;
+    @ViewById(R.id.moreIV) ImageView moreIV;
+    @ViewById(R.id.timeTV) TextView timeTV;
+    @ViewById(R.id.switchSB) SwitchButton switchSB;
+    @ViewById(R.id.daysTV) TextView daysTV;
+
+    @IntArrayRes(R.array.beaconColors)
+    int colors[];
 
     public AlarmItemView(Context context) {
         super(context);
@@ -25,5 +39,11 @@ public class AlarmItemView extends BaseItemView<AlarmItem> {
     @Override
     public void bind(AlarmItem alarmItem) {
         textView.setText(alarmItem.getText());
+        int a = new Random().nextInt(colors.length);
+        sidebar.setBackgroundColor(colors[a]);
+        switchSB.setChecked(new Random().nextBoolean());
+        switchSB.setColor(colors[a]);
+        String time = "" + new Random().nextInt(24) + ":" + (new Random().nextInt(51) + 10);
+        timeTV.setText(time);
     }
 }
