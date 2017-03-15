5b791931bdc1420af42e2fcd00edbd6af9b75309
b00sti <pawlik.dominik@gmail.com>
2017-03-15 13:23:02 +0100

    added notofications

--- app/src/main/java/com/example/b00sti/bbeacon/ui_scanner/ScannerTopFragment.java
+++ app/src/main/java/com/example/b00sti/bbeacon/ui_scanner/ScannerTopFragment.java
@@ -1,18 +1,25 @@
 package com.example.b00sti.bbeacon.ui_scanner;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 
 import com.example.b00sti.bbeacon.R;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
-import org.androidannotations.annotations.FragmentById;
+import org.androidannotations.annotations.res.DrawableRes;
+import org.androidannotations.annotations.res.IntArrayRes;
 
 /**
  * Created by Dominik (b00sti) Pawlik on 2017-03-14
@@ -21,8 +28,14 @@ import org.androidannotations.annotations.FragmentById;
 
 @EFragment(R.layout.scanner_top_fragment)
 public class ScannerTopFragment extends Fragment implements OnMapReadyCallback {
+    private static final String TAG = "ScannerTopFragment";
 
-    @FragmentById(R.id.map) SupportMapFragment supportMapFragment;
+    //@FragmentById(R.id.map) SupportMapFragment supportMapFragment;
+    @IntArrayRes(R.array.beaconColors)
+    int colors[];
+
+    @DrawableRes(R.drawable.ic_place_white_18dp)
+    Drawable markerIcon;
 
     public static ScannerTopFragment newInstance() {
         return new ScannerTopFragment_();
@@ -30,27 +43,79 @@ public class ScannerTopFragment extends Fragment implements OnMapReadyCallback {
 
     @AfterViews
     void init() {
-        //SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        Log.d(TAG, "init: " + supportMapFragment);
         if (supportMapFragment != null) {
             supportMapFragment.getMapAsync(this);
         }
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        LatLng place = new LatLng(50.00, 19.9);
-        googleMap.addMarker(
-                new MarkerOptions()
-                        .position(place)
-                        .title("My BBeacon"));
-
-        LatLng place2 = new LatLng(50.03, 19.93);
-        googleMap.addMarker(
-                new MarkerOptions()
-                        .position(place2)
-                        .title("My BBeacon 2 "));
-
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(place));
+    private BitmapDescriptor getMarkerIconFromDrawable(Drawable drawable) {
+        Canvas canvas = new Canvas();
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        canvas.setBitmap(bitmap);
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        drawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        double lat = 50.00;
+        double lng = 19.9;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            markerIcon.setTint(colors[0]);
+            googleMap.addMarker(
+                    new MarkerOptions()
+                            .position(new LatLng(lat, lng))
+                            .icon(getMarkerIconFromDrawable(markerIcon)));
+
+            lat = lat + 0.025;
+            lng = lng + 0.025;
+            markerIcon.setTint(colors[2]);
+
+            googleMap.addMarker(
+                    new MarkerOptions()
+                            .position(new LatLng(lat, lng))
+                            .icon(getMarkerIconFromDrawable(markerIcon)));
+
+            lat = lat + 0.025;
+            lng = lng + 0.025;
+            markerIcon.setTint(colors[1]);
+
+            googleMap.addMarker(
+                    new MarkerOptions()
+                            .position(new LatLng(lat, lng))
+                            .icon(getMarkerIconFromDrawable(markerIcon)));
+
+            lat = lat - 0.043;
+            lng = lng + 0.01;
+            markerIcon.setTint(colors[2]);
+
+            googleMap.addMarker(
+                    new MarkerOptions()
+                            .position(new LatLng(lat, lng))
+                            .icon(getMarkerIconFromDrawable(markerIcon)));
+
+            lat = lat + 0.03;
+            lng = lng - 0.06;
+            markerIcon.setTint(colors[0]);
+
+            googleMap.addMarker(
+                    new MarkerOptions()
+                            .position(new LatLng(lat, lng))
+                            .icon(getMarkerIconFromDrawable(markerIcon)));
+
+            lat = lat + 0.02;
+            lng = lng + 0.015;
+            markerIcon.setTint(colors[1]);
+
+            googleMap.addMarker(
+                    new MarkerOptions()
+                            .position(new LatLng(lat, lng))
+                            .icon(getMarkerIconFromDrawable(markerIcon)));
+        }
+    }
 }
