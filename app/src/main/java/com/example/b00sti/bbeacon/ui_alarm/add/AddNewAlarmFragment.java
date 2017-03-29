d3a86272447c63269bc5eebd48c7df98cad04a2a
b00sti <pawlik.dominik@gmail.com>
2017-03-29 11:54:42 +0200

    customize add alarm screen

--- app/src/main/java/com/example/b00sti/bbeacon/ui_alarm/add/AddNewAlarmFragment.java
+++ app/src/main/java/com/example/b00sti/bbeacon/ui_alarm/add/AddNewAlarmFragment.java
@@ -4,6 +4,7 @@ import android.app.TimePickerDialog;
 import android.support.v4.app.Fragment;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -12,16 +13,21 @@ import com.example.b00sti.bbeacon.ui_alarm.interactors.SetAlarmInteractor;
 import com.example.b00sti.bbeacon.ui_alarm.main.AlarmItem;
 import com.example.b00sti.bbeacon.utils.RealmUtils;
 import com.example.b00sti.bbeacon.utils.SwitchButton;
+import com.example.b00sti.bbeacon.utils.TimeUtils;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.res.ColorRes;
 import org.androidannotations.annotations.res.IntArrayRes;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 import me.grantland.widget.AutofitTextView;
 
@@ -35,6 +41,7 @@ public class AddNewAlarmFragment extends Fragment {
     @ViewById(R.id.titleET) EditText titleET;
     @ViewById(R.id.switchSB) SwitchButton switchSB;
     @ViewById(R.id.selectedTimeTV) AutofitTextView selectedTimeTV;
+    @ViewById(R.id.timeToNextTV) TextView timeToNextTV;
 
     /*
     @ViewById(R.id.selectBeaconTV) AppCompatButton selectColorB;
@@ -52,6 +59,43 @@ public class AddNewAlarmFragment extends Fragment {
 
     public static AddNewAlarmFragment newInstance() {
         return new AddNewAlarmFragment_();
+    }
+
+    @AfterViews
+    void initUI() {
+        time = getCurrentTime();
+        switchSB.setChecked(true);
+        selectedTimeTV.setText(time);
+        timeToNextTV.setText(getTimeToAlarm());
+    }
+
+    private String getTimeToAlarm() {
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(System.currentTimeMillis());
+
+        int currentHour = cal.get(Calendar.HOUR_OF_DAY);
+        int currentMin = cal.get(Calendar.MINUTE);
+
+        String segments[] = time.split(":");
+        if (Integer.valueOf(segments[0]) < currentHour || Integer.valueOf(segments[0]) == currentHour && Integer.valueOf(segments[1]) < currentMin) {
+            cal.add(Calendar.DAY_OF_YEAR, 1);
+            cal.set(Calendar.HOUR_OF_DAY, Integer.valueOf(segments[0]));
+            cal.set(Calendar.MINUTE, Integer.valueOf(segments[1]));
+        } else {
+            cal.set(Calendar.HOUR_OF_DAY, Integer.valueOf(segments[0]));
+            cal.set(Calendar.MINUTE, Integer.valueOf(segments[1]));
+        }
+
+        String time = TimeUtils.twoDatesBetweenTime(cal.getTimeInMillis());
+
+        return "Time to next alarm: " + time;
+    }
+
+    private String getCurrentTime() {
+        Calendar c = Calendar.getInstance();
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
+
+        return TimeUtils.getTimeWith0(dateFormat.format(c.getTime()));
     }
 
     @Click(R.id.selectBeaconTV)
@@ -84,7 +128,8 @@ public class AddNewAlarmFragment extends Fragment {
         TimePickerDialog.OnTimeSetListener t = new TimePickerDialog.OnTimeSetListener() {
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                 time = hourOfDay + ":" + minute;
-                selectedTimeTV.setText(time);
+                selectedTimeTV.setText(TimeUtils.getTimeWith0(time));
+                timeToNextTV.setText(getTimeToAlarm());
             }
         };
 
